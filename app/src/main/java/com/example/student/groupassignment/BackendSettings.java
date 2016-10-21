b9a22960b8c524db982dafdae8c58747b1e729ff
pokerplayer123 <z969aaron969z@gmail.com>
2016-10-21 18:10:55 +1100

    Backendless Login Implemented

--- app/src/main/java/com/example/student/groupassignment/BackendSettings.java
+++ app/src/main/java/com/example/student/groupassignment/BackendSettings.java
@@ -5,7 +5,7 @@ package com.example.student.groupassignment;
  */
 
 public class BackendSettings {
-    public static final String app_id="GroupAssignment";
+    public static final String app_id="8E1E32AA-F483-C6CE-FFC6-416CAE5B1300";
     public static final String SECRET_KEY="7E171BA1-8523-21C9-FFF1-A353B6944A00";
     public static final String appVersion="v1";
 }
