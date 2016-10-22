eb1727643ff10c5e055a0d4a559e4825d9d5420b
Kevin <iampengi@gmail.com>
2016-10-22 23:34:36 +1100

    Home screen prototype
    # Conflicts:
    #	app/src/main/java/com/example/student/groupassignment/HomeActivity.java

---
+++ app/src/main/java/com/example/student/groupassignment/Activities/ClassActivity.java
@@ -0,0 +1,30 @@
+package com.example.student.groupassignment.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.student.groupassignment.R;
+
+/**
+ * Created by Pengi on 22/10/2016.
+ */
+
+public class ClassActivity extends AppCompatActivity  implements View.OnClickListener {
+    private Button addClass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classes);
+
+        addClass.findViewById(R.id.addClass);
+        addClass.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
