1a1716ccf1f66d31466f99b3e67034e4ff3055b9
pokerplayer123 <z969aaron969z@gmail.com>
2016-10-24 23:04:59 +1100

    Added code for different Objects ZID field Added EventAdapter Working Display of upcoming events Student List Activity Added

--- app/src/main/java/com/example/student/groupassignment/Activities/StudentListActivity.java
+++ app/src/main/java/com/example/student/groupassignment/Activities/StudentListActivity.java
@@ -1,21 +1,109 @@
 package com.example.student.groupassignment.Activities;
 
+import android.app.ListActivity;
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.backendless.Backendless;
+import com.backendless.BackendlessCollection;
+import com.backendless.persistence.BackendlessDataQuery;
+import com.backendless.persistence.QueryOptions;
+import com.example.student.groupassignment.Backendless.LoadingCallback;
 import com.example.student.groupassignment.R;
-import com.example.student.groupassignment.entities.User;
+import com.example.student.groupassignment.adapters.EventAdapter;
+import com.example.student.groupassignment.adapters.StudentAdapter;
+import com.example.student.groupassignment.entities.Event;
+import com.example.student.groupassignment.entities.Users;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Aaron on 24/10/2016.
+ * The StudentListActivity. This the list of Students that have registered to the app
  */
 
-public class StudentListActivity extends RecyclerView.Adapter<User.ViewHolder>{
+public class StudentListActivity extends ListActivity {
+    private BackendlessCollection<Users> users;
+    private StudentAdapter adapter;
+    private List<Users> totalUsers = new ArrayList<>();
+    private boolean isLoadingItems = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_list);
+        adapter = new StudentAdapter( StudentListActivity.this, R.layout.list_item_user, totalUsers);
+        setListAdapter( adapter );
 
 
+        // Query object for searching the backendless datastore to retrieve information
+        QueryOptions queryOptions = new QueryOptions();
+        String whereClause = "Type = 'Student'";
 
-}
+        BackendlessDataQuery query = new BackendlessDataQuery(queryOptions);
+        query.setWhereClause(whereClause);
+        // Retrieve data based from the "User" table
+        Backendless.Persistence.of(Users.class).find(query, new LoadingCallback<BackendlessCollection<Users>>(this, getString(R.string.loading_students)) {
+            @Override
+            public void handleResponse(BackendlessCollection<Users> UsersBackendlessCollection) {
+                users = UsersBackendlessCollection;
+                addMoreItems(UsersBackendlessCollection);
+
+
+                super.handleResponse(UsersBackendlessCollection);
+            }
+        });
+
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (needToLoadItems(firstVisibleItem, visibleItemCount, totalItemCount)) {
+                    isLoadingItems = true;
+
+                    users.nextPage(new LoadingCallback<BackendlessCollection<Users>>(StudentListActivity.this) {
+                        @Override
+                        public void handleResponse(BackendlessCollection<Users> nextPage) {
+
+                            users = nextPage;
+                            addMoreItems(nextPage);
+                            isLoadingItems = false;
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+        /**
+         * Determines whether more items need to be loaded as user scrolls down
+         *
+         * @param firstVisibleItem number of the first item visible on screen
+         * @param visibleItemCount number of items visible on screen
+         * @param totalItemCount   total number of items in list
+         * @return true if user is about to reach the end of a list, else false
+         */
+        private boolean needToLoadItems(int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+            return !isLoadingItems && totalItemCount != 0 && totalItemCount - (visibleItemCount + firstVisibleItem) < visibleItemCount / 2;
+        }
+
+        /**
+         * Adds more items to list and notifies Android that dataset has changed.
+         *
+         * @param nextPage list of new items
+         */
+        private void addMoreItems(BackendlessCollection<Users> nextPage) {
+            totalUsers.addAll(nextPage.getCurrentPage());
+            adapter.notifyDataSetChanged();
+        }
+    }
---
+++ app/src/main/java/com/example/student/groupassignment/entities/Users.java
@@ -0,0 +1,200 @@
+package com.example.student.groupassignment.entities;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessCollection;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.persistence.BackendlessDataQuery;
+
+/**
+ * Created by Aaron on 24/10/2016.
+ * The User Class
+ */
+
+public class Users {
+
+    public String email;
+    public String ZID;
+    public String name;
+    public String type;
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getZid() {
+        return ZID;
+    }
+
+    public void setZid(String zid) {
+        this.ZID = zid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    public Users save()
+    {
+        return Backendless.Data.of( Users.class ).save( this );
+    }
+
+    public Future<Users> saveAsync()
+    {
+        if( Backendless.isAndroid() )
+        {
+            throw new UnsupportedOperationException( "Using this method is restricted in Android" );
+        }
+        else
+        {
+            Future<Users> future = new Future<Users>();
+            Backendless.Data.of( Users.class ).save( this, future );
+
+            return future;
+        }
+    }
+
+    public void saveAsync( AsyncCallback<Users> callback )
+    {
+        Backendless.Data.of( Users.class ).save( this, callback );
+    }
+
+    public Long remove()
+    {
+        return Backendless.Data.of( Users.class ).remove( this );
+    }
+
+    public Future<Long> removeAsync()
+    {
+        if( Backendless.isAndroid() )
+        {
+            throw new UnsupportedOperationException( "Using this method is restricted in Android" );
+        }
+        else
+        {
+            Future<Long> future = new Future<Long>();
+            Backendless.Data.of( Users.class ).remove( this, future );
+
+            return future;
+        }
+    }
+
+    public void removeAsync( AsyncCallback<Long> callback )
+    {
+        Backendless.Data.of( Users.class ).remove( this, callback );
+    }
+
+    public static Users findById( String id )
+    {
+        return Backendless.Data.of( Users.class ).findById( id );
+    }
+
+    public static Future<Users> findByIdAsync( String id )
+    {
+        if( Backendless.isAndroid() )
+        {
+            throw new UnsupportedOperationException( "Using this method is restricted in Android" );
+        }
+        else
+        {
+            Future<Users> future = new Future<Users>();
+            Backendless.Data.of( Users.class ).findById( id, future );
+
+            return future;
+        }
+    }
+
+    public static void findByIdAsync( String id, AsyncCallback<Users> callback )
+    {
+        Backendless.Data.of( Users.class ).findById( id, callback );
+    }
+
+    public static Users findFirst()
+    {
+        return Backendless.Data.of( Users.class ).findFirst();
+    }
+
+    public static Future<Users> findFirstAsync()
+    {
+        if( Backendless.isAndroid() )
+        {
+            throw new UnsupportedOperationException( "Using this method is restricted in Android" );
+        }
+        else
+        {
+            Future<Users> future = new Future<Users>();
+            Backendless.Data.of( Users.class ).findFirst( future );
+
+            return future;
+        }
+    }
+
+    public static void findFirstAsync( AsyncCallback<Users> callback )
+    {
+        Backendless.Data.of( Users.class ).findFirst( callback );
+    }
+
+    public static Users findLast()
+    {
+        return Backendless.Data.of( Users.class ).findLast();
+    }
+
+    public static Future<Users> findLastAsync()
+    {
+        if( Backendless.isAndroid() )
+        {
+            throw new UnsupportedOperationException( "Using this method is restricted in Android" );
+        }
+        else
+        {
+            Future<Users> future = new Future<Users>();
+            Backendless.Data.of( Users.class ).findLast( future );
+
+            return future;
+        }
+    }
+
+    public static void findLastAsync( AsyncCallback<Users> callback )
+    {
+        Backendless.Data.of( Users.class ).findLast( callback );
+    }
+
+    public static BackendlessCollection<Users> find(BackendlessDataQuery query )
+    {
+        return Backendless.Data.of( Users.class ).find( query );
+    }
+
+    public static Future<BackendlessCollection<Users>> findAsync( BackendlessDataQuery query )
+    {
+        if( Backendless.isAndroid() )
+        {
+            throw new UnsupportedOperationException( "Using this method is restricted in Android" );
+        }
+        else
+        {
+            Future<BackendlessCollection<Users>> future = new Future<BackendlessCollection<Users>>();
+            Backendless.Data.of( Users.class ).find( query, future );
+
+            return future;
+        }
+    }
+
+    public static void findAsync( BackendlessDataQuery query, AsyncCallback<BackendlessCollection<Users>> callback )
+    {
+        Backendless.Data.of( Users.class ).find( query, callback );
+    }
+}
